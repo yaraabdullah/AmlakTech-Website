43d4b8c179e7f6383e1e01a37319af6e1a85fa06
Yaraab <159025253+yaraabdullah@users.noreply.github.com>
2025-10-19 23:31:04 +0300

    change font type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
